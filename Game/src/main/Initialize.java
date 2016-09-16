7e0ed5e15ba9a804b04a6f5323b990d337a23910
Daniel Reightneour <dreightn@ycp.edu>
2016-09-16 16:45:04 -0400

    Made the frame, but commented it

--- Game/src/main/Initialize.java
+++ Game/src/main/Initialize.java
@@ -1,10 +1,18 @@
 package main;
 
+import javax.swing.SwingUtilities;
+
 public class Initialize {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//How to draw the client when needed, call something other than map though
+		/*SwingUtilities.invokeLater(new Runnable() {
+		public void run() {
+			Map map = new Map();
+		}
+		});
+		*/
 	}
 
 }
--- Game/src/main/Map.java
+++ Game/src/main/Map.java
@@ -1,5 +1,24 @@
 package main;
+import javax.swing.*;
 
-public class Map {
+public class Map extends JFrame{
+	public Map()
+	{
 
+        JFrame frame = new JFrame("GAME");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        System.out.println("0");
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        //Display the window.
+        //frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        //frame.setUndecorated(true);
+        //frame.setVisible(true);
+        frame.pack();
+        frame.setVisible(true);
+        
+
+	}
 }
