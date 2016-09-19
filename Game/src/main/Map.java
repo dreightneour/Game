3b74c20acd5467777fba2d0ea140b7d68b120769
Daniel Reightneour <dreightn@ycp.edu>
2016-09-18 20:15:45 -0400

    dasfj;d

--- Game/src/main/Map.java
+++ Game/src/main/Map.java
@@ -11,7 +11,7 @@ public class Map extends JPanel{
 	{
 		JFrame frame = new JFrame("GAME");
         //Create and set up the window
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         //JPanel panel = new JPanel();
         this.setSize(500, 500);
@@ -26,20 +26,29 @@ public class Map extends JPanel{
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         
-       System.out.println(this.getSize());
-       repaint();
+        System.out.println(this.getSize());
+       	repaint();
 	}
 
 	
    public void paint(Graphics g){
-		g.setColor(Color.BLUE);
+		/*g.setColor(Color.CYAN);
 		g.fillOval(40, 40, 60, 60); //FOR CIRCLE
 		g.fillRect(80, 30, 200, 200); // FOR SQUARE
 		g.fillRect(200, 100, 100, 200); // FOR RECT
 		
 		g.setColor(Color.BLACK);
+		
 		g.drawOval(40, 40, 60, 60); //FOR CIRCLE
 		g.drawRect(80, 30, 200, 200); // FOR SQUARE
 		g.drawRect(200, 100, 100, 200); // FOR RECT
+		*/
+	   for(int i = 0; i < 10; i++)
+	   {
+		  for(int j = 0; j < i % 5; j++)
+		  {
+			  
+		  }
+	   }
    }
 }
